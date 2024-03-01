aa7bf4b90abea021933fa3ece37b1d203094a2ad
Shakil <shakil.eub.cse@gmail.com>
2024-03-01 19:26:53 +0600

    Database Add

---
+++ Form/3 Dynamic Form with Store/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, redirect, url_for, request
+import pymysql
+
+app = Flask(__name__)
+app.secret_key = '6Lck7P8mAAAAABJdGubIiMM8u8e9lHuSE_KglmTC'
+mysql = pymysql.connect(host='localhost', user='root', password='', db='dynamic_form')
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    data_dict ={}
+    if request.method == 'POST':
+        data_dict =entrydata()
+        studentname = data_dict['studentname']
+        message = "Form Submitted Successfully"
+        return render_template('viewform.html', data = data_dict)
+    return render_template('entryform.html')
+
+def entrydata():
+    
+    data = {}
+    studentname = request.form['studentname']
+    institute = request.form['institute']
+    designation = request.form['designation']
+    course = request.form['course']
+    image_file = request.files['image']
+    imagename = image_file.filename
+    print("Image: ",image_file.filename)
+    if image_file:
+        image_file.save('static/' + imagename)
+
+    data = {'studentname': studentname, 'institute': institute, 'designation': designation, 'course': course, 'imagename': imagename}
+    print(data['studentname'])
+
+    cursor = mysql.cursor()
+    query = "INSERT INTO data_table (studentname, institute, designation, course, imagename) VALUES(%s,%s,%s,%s,%s)"
+    values = (studentname, institute, designation, course, imagename)
+    cursor.execute(query, values)
+    mysql.commit()
+    
+    return data
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
