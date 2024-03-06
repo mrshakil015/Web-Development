e9424073aa05f9c8e8787667a6c9df469d3d8454
Shakil <shakil.eub.cse@gmail.com>
2024-03-06 21:39:04 +0600

    New Database added

---
+++ Rainbow Computer Point Website/database/rainbow_computer (1).sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 06, 2024 at 04:38 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rainbow_computer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_info`
+--
+
+CREATE TABLE `course_info` (
+  `id` int(11) NOT NULL,
+  `courseid` varchar(50) NOT NULL,
+  `course_name` varchar(200) NOT NULL,
+  `month_duration` varchar(50) NOT NULL,
+  `weekly` varchar(20) NOT NULL,
+  `duration_hour` varchar(20) NOT NULL,
+  `duration_minute` varchar(20) NOT NULL,
+  `amount` varchar(11) NOT NULL,
+  `image_name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `course_info`
+--
+
+INSERT INTO `course_info` (`id`, `courseid`, `course_name`, `month_duration`, `weekly`, `duration_hour`, `duration_minute`, `amount`, `image_name`) VALUES
+(3, '102', 'Degital Marketing', '৩', '৩', '১', '৩০', '8888', 'digitalmarketing.jpg'),
+(5, '103', 'Office Application Program', '4', '6', '2', '0', '6000', 'banner.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course_info`
+--
+ALTER TABLE `course_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `course_info`
+--
+ALTER TABLE `course_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
