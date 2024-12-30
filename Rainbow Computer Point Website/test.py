3f2ca258512e30662e89f1d27afaaadaa59f9171
Md. Shakil <shakil.eub.cse@gmail.com>
2024-12-30 23:36:14 +0600

    remove unused data

--- Rainbow Computer Point Website/test.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-import pytz
-
-bangladesh_timezone = pytz.timezone('Asia/Dhaka')
-now_in_bangladesh = datetime.now(bangladesh_timezone)
-admissiondate = now_in_bangladesh.strftime('%d-%m-%Y')
-print("Current date: ",admissiondate)
